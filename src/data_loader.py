eb8270cfa2feb304e375539371901ada228e9bdf
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-10-20 10:59:51 +0530

    Data Loader

---
+++ src/data_loader.py
@@ -0,0 +1,6 @@
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+def get_data_loader(batch_size=32):
+    train_data = datasets.MNIST(root="data", train=True, transform=ToTensor)
+    return DataLoader(train_data, batch_size=batch_size, shuffle=True)
