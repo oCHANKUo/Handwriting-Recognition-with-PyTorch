3b13eb831c7a017922f77baf180d0dbdfc32958f
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-10-20 15:52:09 +0530

    evaluate

---
+++ src/evaluate.py
@@ -0,0 +1,48 @@
+import sys
+import os
+from PIL import Image, ImageOps
+import torch
+from torchvision.transforms import ToTensor
+from model import ImageClassifier
+# from train import ImageClassifier
+import matplotlib.pyplot as plt
+
+def predict_image(image_path, model_path="model_state.pt", device="cpu"):
+    # Load Model
+    model = ImageClassifier().to(device)
+    model.load_state_dict(torch.load(model_path, weights_only=True))
+    model.eval()
+
+    # Load and Process the image
+    with ImageOps.invert(Image.open(image_path).convert('L')) as img:
+        img_tensor = ToTensor()(img).unsqueeze(0).to(device)
+
+    # Perform Prediction
+    with torch.no_grad():
+        output = model(img_tensor)
+        predicted_label = torch.argmax(output).item()
+
+    # Show image with prediction
+    plt.imshow(img, cmap="gray")
+    plt.title(f"Predicted Label: {predicted_label}")
+    plt.axis('off')
+    plt.savefig(f"{image_path}_prediction.png")
+    plt.show()
+
+    print(f"Predicted Label: {predicted_label}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python evaluate.py <image_path>")
+        sys.exit(1)
+
+    image_path = sys.argv[1]
+    if not os.path.exists(image_path):
+        print(f"Error: File '{image_path}' not found.")
+        sys.exit(1)
+
+    predict_image(image_path)
+
+
+    # Run this script
+    # python src/evaluate.py digits/digit1.png --- parameter is the location of the image
